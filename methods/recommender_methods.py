0a1c916839be38e1f4e42fd4853d61ad295191b8
Ma7i5ab3 <mattia.sabella1998@gmail.com>
2025-04-03 00:22:25 +0200

    feat(ecoFL Platform as a Service): added Service Feature to be run locally

--- methods/recommender_methods.py
+++ methods/recommender_methods.py
@@ -1,15 +1,15 @@
-from data_quality import *
+from data_processing.data_quality import *
 
 
 # We rank the nodes based on a score resulting from weighting the emission and the quality of the data. The volume of the data is analyzed after
 def ranking_nodes(single_node, max_emi, weight_emi, weight_cons, weight_com):
     emissions = round(
-        single_node["carbon_intensity"] * single_node["energy_consumption"], 2
+        single_node["energy"]["carbon_intensity"] * single_node["energy"]["energy_consumption"], 2
     )  # Quantity of emission per hour
     score = (
         (weight_emi * (1 - (emissions / max_emi)))
-        + (weight_cons * single_node["consistency"])
-        + (weight_com * single_node["completeness"])
+        + (weight_cons * single_node["data"]["data_consistency"]["data_quality_dimension_percentage"])
+        + (weight_com * single_node["data"]["data_completeness"]["data_quality_dimension_percentage"])
     )
 
     return score
@@ -17,29 +17,28 @@ def ranking_nodes(single_node, max_emi, weight_emi, weight_cons, weight_com):
 
 # Node Selection Method
 def nodes_selection(
-    data,
-    weight_emission,
-    weight_consistency,
-    weight_completeness,
-    n_clients,
-    data_volume,
+    node_options_lst: list,
+    num_clients_pred: int,
+    weight_emission: float = 0.7,
+    weight_consistency: float = 0.2,
+    weight_completeness: float = 0.1
 ):
 
     # Total number of clients available
-    n_total_clients = len(data)
+    n_total_clients = len(node_options_lst)
 
     # Weight are normalized
     weight = weight_emission + weight_consistency + weight_completeness
     # Take the maximum to normalize the emissions
     emissions = []
-    for node in data:
+    for node in node_options_lst:
         emissions.append(
-            round(node["carbon_intensity"] * node["energy_consumption"], 2)
+            round(node['energy']["carbon_intensity"] * node['energy']["energy_consumption"], 2)
         )
     max_emission = max(emissions)
     if weight <= 1.0:
         data_sorted = sorted(
-            data,
+            node_options_lst,
             key=lambda x: ranking_nodes(
                 x,
                 max_emission,
@@ -51,13 +50,13 @@ def nodes_selection(
         )
 
     # Data_volume per each client requested
-    data_volume_node = round(((1.0 / n_total_clients) * data_volume), 2)
+    data_volume_node = round((1.0 / n_total_clients), 2)
 
     # Check if a node has the data_volume requested otherwise do not select it and select the next node
     nodes_data_suc = []
     nodes_data_last_suc = []
     for obj_exp in data_sorted:
-        if data_volume_node < obj_exp["data_volume"]:
+        if data_volume_node < obj_exp["data"]["data_volume"]["data_quality_dimension_percentage"]:
             nodes_data_suc.append(obj_exp)
         else:
             nodes_data_last_suc.append(obj_exp)
@@ -66,42 +65,36 @@ def nodes_selection(
     nodes_data_suc.extend(nodes_data_last_suc)
 
     # This is the list of the selected nodes
-    nodes = nodes_data_suc[:n_clients]
+    nodes = nodes_data_suc[:num_clients_pred]
 
     return nodes
 
 
 # Smart Reduction Method
 def nodes_selection_smart(
-    data,
-    weight_emission,
-    weight_consistency,
-    weight_completeness,
-    n_clients,
-    data_volume,
-    x_train,
-    y_train,
-    x_val,
-    y_val,
-    indexes_train,
-    indexes_val,
+    node_options_lst: list,
+    num_clients_pred: int,
+    data: dict,
+    weight_emission: float = 0.7,
+    weight_consistency: float = 0.2,
+    weight_completeness: float = 0.1
 ):
 
     # Total number of clients available
-    n_total_clients = len(data)
+    n_total_clients = len(node_options_lst)
 
     # Weight are normalized
     weight = weight_emission + weight_consistency + weight_completeness
     # Take the maximum to normalize the emissions
     emissions = []
-    for node in data:
+    for node in node_options_lst:
         emissions.append(
-            round(node["carbon_intensity"] * node["energy_consumption"], 2)
+            round(node["energy"]["carbon_intensity"] * node["energy"]["energy_consumption"], 2)
         )
     max_emission = max(emissions)
     if weight <= 1.0:
         data_sorted = sorted(
-            data,
+            node_options_lst,
             key=lambda x: ranking_nodes(
                 x,
                 max_emission,
@@ -113,15 +106,15 @@ def nodes_selection_smart(
         )
 
     # Data_volume per each client requested
-    data_volume_node = round(((1.0 / n_total_clients) * data_volume), 2)
+    data_volume_node = round((1.0 / n_total_clients), 2)
     # Total data volume requested
-    data_volume_total = round((data_volume_node * n_clients), 2)
+    data_volume_total = round((data_volume_node * num_clients_pred), 2)
 
     # Check if a node has the data_volume requested otherwise do not select it and select the next node
     nodes_data_suc = []
     nodes_data_last_suc = []
     for obj_exp in data_sorted:
-        if data_volume_node < obj_exp["data_volume"]:
+        if data_volume_node < obj_exp["data"]["data_volume"]["data_quality_dimension_percentage_overall"]:
             nodes_data_suc.append(obj_exp)
         else:
             nodes_data_last_suc.append(obj_exp)
@@ -130,9 +123,9 @@ def nodes_selection_smart(
     nodes_data_suc.extend(nodes_data_last_suc)
 
     # List of the selected nodes
-    active_nodes = nodes_data_suc[:n_clients]
+    active_nodes = nodes_data_suc[:num_clients_pred]
     # List of future candidates
-    candidates = nodes_data_suc[n_clients:]
+    candidates = nodes_data_suc[num_clients_pred:]
     print(f"Number active nodes {len(active_nodes)}")
 
     data_volume_sum = 0
@@ -142,9 +135,9 @@ def nodes_selection_smart(
     # Smart Reduction of the datasets
     for node in active_nodes:
         final_percentage = compute_effective_percentage(
-            node, x_train, y_train, x_val, y_val, indexes_train, indexes_val
+            node, data['X_train'], data['y_train'], data['X_val'], data['y_val'], data['indexes_train'], data['indexes_val']
         )
-        print(f"Final percentage of node {node['id']} is {final_percentage}")
+        print(f"Final percentage of node {node['node_id']} is {final_percentage}")
         if data_volume_sum < data_volume_total and final_percentage > 0.0:
             data_volume_sum += final_percentage
             percentages.append(final_percentage)
@@ -160,9 +153,9 @@ def nodes_selection_smart(
     while data_volume_sum < data_volume_total:
         print(f"Data volume sum: {data_volume_sum}, {i}, {len(active_nodes)}")
         obj = candidates[i]
-        if (data_volume_total - data_volume_sum) < obj["data_volume"]:
+        if (data_volume_total - data_volume_sum) < obj["data"]["data_volume"]["data_quality_dimension_percentage_overall"]:
             final_percentage_second = compute_effective_percentage(
-                obj, x_train, y_train, x_val, y_val, indexes_train, indexes_val
+                obj, data['X_train'], data['y_train'], data['X_val'], data['y_val'], data['indexes_train'], data['indexes_val']
             )
             if final_percentage_second > 0.00:
                 print(f"Final percentage: {final_percentage_second}")
@@ -182,7 +175,7 @@ def nodes_selection_smart(
         print(f"Data volume sum: {data_volume_sum}, {i}, {len(active_nodes)}")
         obj = candidates[i]
         final_percentage_second = compute_effective_percentage(
-            obj, x_train, y_train, x_val, y_val, indexes_train, indexes_val
+            obj, data['X_train'], data['y_train'], data['X_val'], data['y_val'], data['indexes_train'], data['indexes_val']
         )
         if final_percentage_second > 0.00:
             print(f"Final percentage: {final_percentage_second}")
@@ -198,35 +191,29 @@ def nodes_selection_smart(
 
 # Minimal Smart Reduction Method
 def nodes_selection_smart_minimal(
-    data,
-    weight_emission,
-    weight_consistency,
-    weight_completeness,
-    n_clients,
-    data_volume,
-    x_train,
-    y_train,
-    x_val,
-    y_val,
-    indexes_train,
-    indexes_val,
+    node_options_lst: list,
+    num_clients_pred: int,
+    data: dict,
+    weight_emission: float = 0.7,
+    weight_consistency: float = 0.2,
+    weight_completeness: float = 0.1
 ):
 
     # Total number of clients available
-    n_total_clients = len(data)
+    n_total_clients = len(node_options_lst)
 
     # Weight are normalized
     weight = weight_emission + weight_consistency + weight_completeness
     # Take the maximum to normalize the emissions
     emissions = []
-    for node in data:
+    for node in node_options_lst:
         emissions.append(
-            round(node["carbon_intensity"] * node["energy_consumption"], 2)
+            round(node["energy"]["carbon_intensity"] * node["energy"]["energy_consumption"], 2)
         )
     max_emission = max(emissions)
     if weight <= 1.0:
         data_sorted = sorted(
-            data,
+            node_options_lst,
             key=lambda x: ranking_nodes(
                 x,
                 max_emission,
@@ -238,15 +225,13 @@ def nodes_selection_smart_minimal(
         )
 
     # Data_volume per each client requested
-    data_volume_node = round(((1.0 / n_total_clients) * data_volume), 2)
-    # Total data volume requested
-    data_volume_total = round((data_volume_node * n_clients), 2)
+    data_volume_node = round((1.0 / n_total_clients), 2)
 
     # Check if a node has the data_volume requested otherwise do not select it and select the next node
     nodes_data_suc = []
     nodes_data_last_suc = []
     for obj_exp in data_sorted:
-        if data_volume_node < obj_exp["data_volume"]:
+        if data_volume_node < obj_exp["data"]["data_volume"]["data_quality_dimension_percentage_overall"]:
             nodes_data_suc.append(obj_exp)
         else:
             nodes_data_last_suc.append(obj_exp)
@@ -255,29 +240,23 @@ def nodes_selection_smart_minimal(
     nodes_data_suc.extend(nodes_data_last_suc)
 
     # List of the selected nodes
-    active_nodes = nodes_data_suc[:n_clients]
+    active_nodes = nodes_data_suc[:num_clients_pred]
     print(f"Number active nodes {len(active_nodes)}")
 
     percentages = []
     ultimate_nodes = []
     for node in active_nodes:
         percentage = compute_effective_percentage(
-            node, x_train, y_train, x_val, y_val, indexes_train, indexes_val
+            node, data['X_train'], data['y_train'], data['X_val'], data['y_val'], data['indexes_train'], data['indexes_val']
         )
         if percentage > 0.0:
             percentages.append(percentage)
             ultimate_nodes.append(node)
         else:
             percentage = compute_effective_percentage(
-                nodes_data_suc[n_clients],
-                x_train,
-                y_train,
-                x_val,
-                y_val,
-                indexes_train,
-                indexes_val,
+                nodes_data_suc[num_clients_pred], data['X_train'], data['y_train'], data['X_val'], data['y_val'], data['indexes_train'], data['indexes_val']
             )
             percentages.append(percentage)
-            ultimate_nodes.append(nodes_data_suc[n_clients])
+            ultimate_nodes.append(nodes_data_suc[num_clients_pred])
 
     return ultimate_nodes, percentages
